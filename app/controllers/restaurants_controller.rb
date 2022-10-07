d9207347793176431d40442306a1b1612950ffdb
Claire Lin <chunhuilin7516@gmail.com>
2022-10-07 17:25:47 +0800

    add chef to the restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,14 +1,24 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :set_restaurant, only: %i[show edit update destroy chef]
 
   # GET /restaurants
   def index
     @restaurants = Restaurant.all
   end
 
+  def top
+    @restaurants = Restaurant.where(rating: 5)
+  end
+
+  def chef
+    @chef_name = @restaurant.chef_name
+  end
+
   # GET /restaurants/1
   def show
+
   end
+
 
   # GET /restaurants/new
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :restaurants
   resources :restaurants do
     collection do
       get :top  # get "top", to: "restaurants#top", as: :top_restaurants
+    end
+    member do
+      get :chef
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
