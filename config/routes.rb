f1dead08dbfe7ebf5cbc60a141fc38150259f3ef
Claire Lin <chunhuilin7516@gmail.com>
2022-10-07 15:08:19 +0800

    scaffold restaurants 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :restaurants
+  resources :restaurants do
+    collection do
+      get :top  # get "top", to: "restaurants#top", as: :top_restaurants
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
